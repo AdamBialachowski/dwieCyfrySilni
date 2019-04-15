e2026c425e8804fba40eec71b5dd16467373f3c8
AdamBialachowski <pitoab@wp.pl>
2019-04-15 21:53:50 +0200

    nie działa na spoj

--- src/dwieCyfrySilni.kt
+++ src/dwieCyfrySilni.kt
@@ -6,39 +6,37 @@ fun readValue( numbersToCheck:IntArray){// wczytuje liczby do sprawdzenia
     }
 }
 
-fun silnia(data:Int):Int{//oblicza silnie
+fun silnia(data:Int):String{//oblicza silnie
     var wynik:Int = 1
     for (i in 1..data){
         wynik = wynik * i
-        //println(wynik)
+    }
+    return wynik.toString()
+}
+
+fun printTwoNumbers(number :String):String{//wyświetla liczbe dziesiątek spacja liczbe jedności
+    var wynik: String = "0"
+    for (i in 0..number.length){
+        if (i == number.length-2)wynik = number[i].toString()
+        if (i == number.length-1) wynik = wynik + " " + number[i].toString()
     }
     return wynik
 }
 
-fun printTwoNumbers(number :String){//wyświetla liczbe dziesiątek spacja liczbe jedności
-    for (i in 0..number.length){
-        if (i == number.length-2)print(number[i])
-        if (i == number.length-1)print(" "+number[i])
-    }
-}
-
-fun writeScore(numbersToWrite: IntArray){
+fun writeScore(numbersToWrite: IntArray){//wypisuje wyniki
     for (i in 0..(numbersToWrite.size-1)){
-        //println(silnia(numbersToWrite[i]))
-        //var numberStr :String = silnia(numbersToWrite[i]).toString()
-        //println(printTwoNumbers(silnia(numbersToWrite[i]).toString()))
-        println(printTwoNumbers(silnia(numbersToWrite[i]).toString()))
-        println((silnia(numbersToWrite[i]).toString()))
+        println(printTwoNumbers(silnia(numbersToWrite[i])))
     }
 }
 
 /////////////////////////////////////////////////////////////////////////////////
+
 fun main(args:Array<String>) {
     //Variables/////////////////////////////////////////////
     val howMutchTest = readLine().toString().toInt()
     var arrayOfNumbers = IntArray(howMutchTest)
     ////////////////////////////////////////////////////////
+
     readValue(arrayOfNumbers)
     writeScore(arrayOfNumbers)
-    //printTwoNumbers(howMutchTest.toString())
 }
